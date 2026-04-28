1529c7fed4f9798611872269fde2b67d3405f22b
unknown <EdwardAdventusD>
2026-04-28 21:50:18 +0700

    Update modul 9

---
+++ Controllers/MahasiswaController.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using tpmodul9_103082400028.Models;
+
+namespace tpmodul9_103082400028.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class MahasiswaController : ControllerBase
+    {
+        private static List<Mahasiswa> daftarMahasiswa = new List<Mahasiswa>
+        {
+            new Mahasiswa
+            {
+                Nama = "Edward Adventus Dembo",
+                Nim = "103082400028"
+            }
+        };
+
+        // GET: api/mahasiswa
+        [HttpGet]
+        public ActionResult<IEnumerable<Mahasiswa>> GetAll()
+        {
+            return Ok(daftarMahasiswa);
+        }
+
+        // GET: api/mahasiswa/0
+        [HttpGet("{index}")]
+        public ActionResult<Mahasiswa> GetByIndex(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+            {
+                return NotFound("Data mahasiswa tidak ditemukan");
+            }
+
+            return Ok(daftarMahasiswa[index]);
+        }
+
+        // POST: api/mahasiswa
+        [HttpPost]
+        public ActionResult AddMahasiswa([FromBody] Mahasiswa mahasiswa)
+        {
+            daftarMahasiswa.Add(mahasiswa);
+            return Ok("Mahasiswa berhasil ditambahkan");
+        }
+
+        // DELETE: api/mahasiswa/0
+        [HttpDelete("{index}")]
+        public ActionResult DeleteMahasiswa(int index)
+        {
+            if (index < 0 || index >= daftarMahasiswa.Count)
+            {
+                return NotFound("Data mahasiswa tidak ditemukan");
+            }
+
+            daftarMahasiswa.RemoveAt(index);
+            return Ok("Mahasiswa berhasil dihapus");
+        }
+    }
+}
---
+++ Models/Mahasiswa.cs
@@ -0,0 +1,8 @@
+﻿namespace tpmodul9_103082400028.Models
+{
+    public class Mahasiswa
+    {
+        public string Nama { get; set; }
+        public string Nim { get; set; }
+    }
+}
